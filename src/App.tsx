f6a7cf4bb1c8a9e99c07a7b145cce4c8f33d1ffc
= <=>
2022-09-19 12:16:17 -0500

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 
 import './App.css';
 import "./Fonts.css";
-<<<<<<< HEAD
 import ModalCrearUsuario from "../src/components/ModalCrearEditarUsuario/ModalCrearUsuario"
 import ModalEditarUsuario from "../src/components/ModalCrearEditarUsuario/ModalModificarUsuario"
-=======
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import { Home } from './screens/home/Home';
-import { Settings } from './screens/config/Settings';
-import { E404 } from './screens/e404/E404';
-import { Init } from './screens/init/Init';
-
->>>>>>> ef96f529d41098ec2e73d9138eee68eece8814aa
 
 
 function App() {
@@ -20,22 +11,9 @@ function App() {
 
 
   return (
-<<<<<<< HEAD
 
     <ModalCrearUsuario></ModalCrearUsuario>
 
-=======
-   <>
-    <Router>
-        <Routes>
-          <Route index element={<Init />} />
-          <Route path="home" element={<Home />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="*" element={<E404 />} />
-        </Routes>
-      </Router>
-   </>
->>>>>>> ef96f529d41098ec2e73d9138eee68eece8814aa
   );
 }
 
