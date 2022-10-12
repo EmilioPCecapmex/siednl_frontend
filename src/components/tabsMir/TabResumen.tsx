a5d1bdbb404164cfcffe1fc2c514351eebe8702d
MTYAlonso <japerez@cecapmex.com>
2022-10-12 10:32:53 -0500

    tab resumen modificado

--- src/components/tabsMir/TabResumen.tsx
+++ src/components/tabsMir/TabResumen.tsx
@@ -21,7 +21,7 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import axios from "axios";
 
 export function TabResumen({ show }: { show: boolean }) {
-  const [componentes, setComponentes] = useState([1, 2, 3]);
+  const [componentes, setComponentes] = useState([1, 2, 3, 4, 5, 6]);
   const [actividades, setActividades] = useState([1, 2, 3, 4, 5, 6]);
 
   const [componentSelect, setComponentSelect] = useState(1);
@@ -48,20 +48,20 @@ export function TabResumen({ show }: { show: boolean }) {
         sx={{
           width: "10vw",
           height: "85vh",
-          borderRight: "solid",
+          borderRight: "1px solid",
           display: "flex",
           flexDirection: "column",
           borderColor: "#BCBCBC",
-          overflow:'hidden',
-          overflowY:'scroll',
-          "&::-webkit-scrollbar": {
-            width: ".3vw",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "rgba(0,0,0,.5)",
-            outline: "1px solid slategrey",
-            borderRadius: 10,
-          },
+          // overflow:'hidden',
+          // overflowY:'scroll',
+          // "&::-webkit-scrollbar": {
+          //   width: ".3vw",
+          // },
+          // "&::-webkit-scrollbar-thumb": {
+          //   backgroundColor: "rgba(0,0,0,.5)",
+          //   outline: "1px solid slategrey",
+          //   borderRadius: 10,
+          // },
         }}
       >
         <ListItemButton
@@ -162,57 +162,12 @@ export function TabResumen({ show }: { show: boolean }) {
                   }}
                 >
                   <ListItemText primary={`Componente ${item}`} />
-                  <ListItemButton
-                    key={103}
-                    selected={  item === componentSelect  ? true : false}
-                    onClick={() => {
-                      setOpenActividades(activitySelect === componentSelect  ? !openActividades : false);
-                    }}
-                    sx={{
-                      fontFamily: "MontserratMedium",
-                      "&.Mui-selected ": {
-                        backgroundColor: "#c4a57b",
-                      },
-                      "&.Mui-selected:hover": {
-                        backgroundColor: "#cbcbcb",
-                      },
-                    }}
-                  >
-                    {openActividades ? <ExpandLess /> : <ExpandMore />}
-                  </ListItemButton>
-                  <Collapse in={openActividades} timeout="auto" unmountOnExit> 
-                  {/* in={openActividades}  */}
-                    <List disablePadding>
-                      {actividades.map((item2) => {
-                        return (
-                          <ListItemButton
-                            selected={item2 === activitySelect && item === componentSelect ? true : false}
-                            key={item2}
-                            onClick={() => {
-                              setActivitySelect(item2);
-                            }}
-                            sx={{
-                              "&.Mui-selected ": {
-                                backgroundColor: "#c4a57b",
-                              },
-                              "&.Mui-selected:hover": {
-                                backgroundColor: "#cbcbcb",
-                              },
-                            }}
-                          >
-                            <ListItemText primary={`Actividad ${item2}`} />
-                          </ListItemButton>
-                        );
-                      })}
-                    </List>
-                  </Collapse>
+                 
                 </ListItemButton>
               );
             })}
           </List>
         </Collapse>
-        
-        <Divider />
       </List>
     </Box>
   );
