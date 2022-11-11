b84974e717b3c10c02cf11ebf76ecb6c58833ac1
= <=>
2022-11-11 17:31:12 -0600

    Archivo se descarga con su nombre

--- src/components/catalogos/CSVCatalogo.tsx
+++ src/components/catalogos/CSVCatalogo.tsx
@@ -1,30 +1,73 @@
 import { Dialog, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import logoExcell from "../../assets/img/xlsx_Logo.png"
+import logoExcell from "../../assets/img/xlsx_Logo.png";
+import { Catalogos } from "./Catalogos";
 
-export const CSVCatalogo = ({
-    tabla,
-}:{
-    tabla:string,
-}) => {
-    const [open, setOpen] = React.useState(false);
+export const CSVCatalogo = ({ tabla }: { tabla: string }) => {
+
+  const download = (data: any) => {
+    const blob = new Blob([data], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.setAttribute("href", url);
+    a.setAttribute("download", `${tabla}.csv`);
+    a.click();
+  };
+
+  const csvmaker = (data: any) => {
+    let csvRows = [];
+
+    const headers = Object.keys(data[0]);
+    csvRows.push(headers.join(","));
+    console.log(csvRows);
+    let values = {};
+    for(let i = 0; i<data.length; i++){
+    values = Object.values(data[i]).join(",");
+    csvRows.push(values);
+    }
+
+    return csvRows.join("\n");
+  };
+
+  const get = () => {
     
-    const handleClose = () => {
-        setOpen(false);
-      };
-
+    const data = [{
+        id: 1,
+        name: "Geeks",
+        profession: "developer",
+      },
+    {
+        id: 2,
+        name: "Geeks",
+        profession: "developer", 
+    },
+    {
+        id: 3,
+        name: "Geeks",
+        profession: "developer", 
+    },
+    {
+        id: 4,
+        name: "Geeks",
+        profession: "developer", 
+    }
+];
+    
      
-  
-        return (
-          <>
-            
-            
-         <img src={logoExcell} alt="Logo" style={{ width:"2.5vw", height:"3.5vh"}}  onClick={()=>setOpen(true)}/>
-         <Dialog open={open} onClose={handleClose} fullWidth>
-            {tabla}
-            </Dialog>
-         </>
-        );
-      
-}
+    const csvdata = csvmaker(data)
+    download(csvdata);
+  };
+
+  <Catalogos defSelected=""/>
+  return (
+    <>
+      <img
+        src={logoExcell}
+        alt="Logo"
+        style={{ width: "2.5vw", height: "3.5vh" }}
+        onClick={() => get()}
+      />
+    </>
+  );
+};
