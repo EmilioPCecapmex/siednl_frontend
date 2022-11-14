1dea571605eb41c30bcc72478f61ebc744d768a7
= <=>
2022-11-14 12:49:06 -0600

    Instituciones se pasan a scv

--- src/components/catalogos/CSVCatalogo.tsx
+++ src/components/catalogos/CSVCatalogo.tsx
@@ -1,10 +1,15 @@
 import { Dialog, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import axios from "axios";
+import * as React from "react";
 import logoExcell from "../../assets/img/xlsx_Logo.png";
 import { Catalogos } from "./Catalogos";
 
 export const CSVCatalogo = ({ tabla }: { tabla: string }) => {
+
+  const [catalogoInstituciones, setCatalogoInstituciones] = React.useState([
+    { Id: "", NombreInstitucion: "" },
+  ]);
 
   const download = (data: any) => {
     const blob = new Blob([data], { type: "text/csv" });
@@ -15,49 +20,78 @@ export const CSVCatalogo = ({ tabla }: { tabla: string }) => {
     a.click();
   };
 
-  const csvmaker = (data: any) => {
+  const csvmaker = (data: any, headerNumber: number) => {
     let csvRows = [];
 
+    if(tabla === "Instituciones"){
     const headers = Object.keys(data[0]);
-    csvRows.push(headers.join(","));
+    csvRows.push(headers[headerNumber]);
     console.log(csvRows);
+    
     let values = {};
     for(let i = 0; i<data.length; i++){
-    values = Object.values(data[i]).join(",");
+    values = data[i].NombreInstitucion;
     csvRows.push(values);
     }
 
     return csvRows.join("\n");
+  }
   };
 
   const get = () => {
-    
-    const data = [{
-        id: 1,
-        name: "Geeks",
-        profession: "developer",
-      },
-    {
-        id: 2,
-        name: "Geeks",
-        profession: "developer", 
-    },
-    {
-        id: 3,
-        name: "Geeks",
-        profession: "developer", 
-    },
-    {
-        id: 4,
-        name: "Geeks",
-        profession: "developer", 
-    }
-];
-    
-     
-    const csvdata = csvmaker(data)
+    const csvdata = csvmaker(catalogoInstituciones,1)
     download(csvdata);
   };
+
+  const getAniosFiscales = () => {
+    axios
+      .get(process.env.REACT_APP_APPLICATION_BACK + "/api/AniosFiscales", {
+        headers: {
+          Authorization: localStorage.getItem("jwtToken") || "",
+        },
+      })
+      .then((r) => {
+        if (r.status === 200) {
+          let update = r.data.data;
+          update = update.map(
+            (item: { Id: string; AnioFiscal: string; Tabla: string }) => {
+              return {
+                Id: item.Id,
+                Desc: item.AnioFiscal,
+                Tabla: "AniosFiscales",
+              };
+            }
+          );
+        }
+      });
+  };
+
+
+  const getInstituciones = () => {
+    axios
+      .get(process.env.REACT_APP_APPLICATION_BACK + "/api/instituciones", {
+        headers: {
+          Authorization: localStorage.getItem("jwtToken") || "",
+        },
+        params: {
+
+          IdUsuario: localStorage.getItem("IdUsuario"),
+
+          IdInstitucion: localStorage.getItem("IdInstitucion")
+
+        },
+      })
+      .then((r) => {
+        setCatalogoInstituciones(r.data.data);
+      });
+  };
+
+  
+  React.useEffect(() => {
+    getInstituciones();
+
+  }, []);
+  
 
   <Catalogos defSelected=""/>
   return (
