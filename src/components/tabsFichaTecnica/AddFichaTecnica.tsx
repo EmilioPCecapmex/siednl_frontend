d9e867ddbd8a8024f46a7920b0cec86e0ab9e455
= <=>
2022-11-25 11:09:07 -0600

    Vista de botones de Tabs

--- src/components/tabsFichaTecnica/AddFichaTecnica.tsx
+++ src/components/tabsFichaTecnica/AddFichaTecnica.tsx
@@ -4,6 +4,10 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { Tooltip } from "@mui/material";
 import { TabEncabezado } from "./TabEncabezado";
+import { TabFinProposito } from "./TabFinProposito";
+import { TabComponentes } from "./TabComponentes";
+import { TabActividades } from "./TabActividades";
+import { TabResumen } from "./TabResumen";
 
 
 export default function FullModalFichaTecnica({
@@ -125,7 +129,46 @@ export default function FullModalFichaTecnica({
             asignarComponenteValor={()=>{}}
             fichaTecnica={""}
           ></TabEncabezado>
+            <TabFinProposito
+            show={value === 20 ? true : false}
+            resumenFin={()=>{}}
+            resumenProposito={()=>{}}
+            cargaFin={[]}
+            cargaProposito={[]}
+            mirEdit={MIR ? JSON.parse(MIR)[1] : null}
+          ></TabFinProposito>
 
+          <TabComponentes
+            show={value === 30 ? true : false}
+            noComponentesFnc={()=>{}}
+            valoresComponenteFnc={()=>{}}
+            noComponentes={[]}
+            valoresComponente={[]}
+            mirEdit={MIR ? JSON.parse(MIR)[1] : null}
+          ></TabComponentes>
+
+          <TabActividades
+            show={value === 40 ? true : false}
+            actividadesFichaTecnica={[]}
+            componentesTextos={[]}
+            componenteActividad={[]}
+            componentes={[]}
+            asignarCValor={()=>{}}
+            fichaTecnicaEdit={MIR ? JSON.parse(MIR)[1] : null}
+          ></TabActividades>
+
+          <TabResumen
+            show={value === 50 ? true : false}
+            showResume={()=>{}}
+            fichaTecnicaEdit={MIR ? JSON.parse(MIR)[1] : null}
+            componentes={[]}
+            componenteValor={[]}
+            cValor={[]}
+            encabezado={[]}
+            fin={[]}
+            proposito={[]}
+            IdFichaTecnica={""}
+          ></TabResumen>
         </Box>
       </Box>
     </Box>
