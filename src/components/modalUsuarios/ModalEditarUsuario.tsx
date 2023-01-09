49749e1ed3dbfeb981312d8cd6cd5516bfeef2b6
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2023-01-09 13:27:00 -0600

    avisos actualizados

--- src/components/modalUsuarios/ModalEditarUsuario.tsx
+++ src/components/modalUsuarios/ModalEditarUsuario.tsx
@@ -230,7 +230,7 @@ export default function ModalEditarUsuario({
            
             Toast.fire({
               icon: "success",
-              title: "¡Registro exitoso!",
+              title: r.data.data[0][0].Mensaje,
             });
           
           handleClose();
