5ea3e6264e03d42872661842c9433d512104b208
SeñordecapituloIsrael <Noreply@cecapmex.com>
2023-01-24 12:12:18 -0600

    tutorial box

--- src/funcs/sendMailCustomMessage.tsx
+++ src/funcs/sendMailCustomMessage.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { json } from "stream/consumers";
 
 export const sendMailCustomMessage = (v: string,mensaje: string, documentType: string, ) => {
   axios.post(
@@ -9,6 +10,28 @@ export const sendMailCustomMessage = (v: string,mensaje: string, documentType: s
       //"d4b35a67-5eb9-11ed-a880-040300000000" gonzzo
       IdDestinatario: v,
       IdRemitente: localStorage.getItem("IdUsuario"),
+    },
+    {
+      headers: {
+        Authorization: localStorage.getItem("jwtToken") || "",
+      },
+    }
+  );
+};
+
+export const sendMail = (v: string,mensaje: string, documentType: string, ) => {
+  axios.post(
+    "http://10.200.4.105:8001/api/serviciocorreo",
+    {
+      referencia: "009",
+      subject: `  Avances con la ${documentType}`,
+
+      data: JSON.parse(mensaje + documentType)
+      
+      `${mensaje} ${documentType}`,//json
+      //"d4b35a67-5eb9-11ed-a880-040300000000" gonzzo
+      IdDestinatario: v,// Aqui va un array
+      IdRemitente: localStorage.getItem("IdUsuario"),//Aqui tambien es un array
     },
     {
       headers: {
