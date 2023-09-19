b4739b0a301bc28a1f4bc54556c25d3fe999fbd5
beto081091 <beto_081091@hotmail.com>
2023-09-19 10:55:10 -0600

    Actualización PAE

--- src/components/tabsPAE/TabPAE.tsx
+++ src/components/tabsPAE/TabPAE.tsx
@@ -20,14 +20,17 @@ import {
   Collapse,
   Typography,
   Grid,
+  TableSortLabel,
 } from "@mui/material";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useEffect, useState } from "react";
 import { InsertarComponentePDF } from "./InsertarPDF";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
+import LaunchIcon from '@mui/icons-material/Launch';
+import DownloadIcon from "@mui/icons-material/Download";
 import axios from "axios";
 import Swal from "sweetalert2";
+
 
 export const TabPAE = ({
   anios,
@@ -84,6 +87,31 @@ export const TabPAE = ({
     FechaCreacion: string;
     Deleted: number;
   }
+
+  interface Head {
+    id: string;
+    isNumeric: boolean;
+    label: string;
+  }
+  
+  const heads: readonly Head[] = [
+    {
+      id: "Titulo",
+      isNumeric: true,
+      label: "TÍTULO ARCHIVO",
+    },
+    
+    {
+      id: "Opciones",
+      isNumeric: true,
+      label: "OPCIONES",
+    },
+  ];
+  
+  const [page, setPage] = useState(0);
+  const renglonesPagina = 6;
+  const [rowsPerPage, setRowsPerPage] = useState(renglonesPagina);
+  const [mirsFiltered, setMirsFiltered] = useState<Array<IIPAE>>([]);
 
   const creaPAE = (Anio: string, Numero: string, Nombre: string, Ruta: string) => {
     axios
@@ -326,7 +354,7 @@ export const TabPAE = ({
                       sx={{ fontFamily: "MontserratMedium", fontSize: "0.7vw" }}
                     >
                       {item == 1 ? "2022" : item == 2 ? "2021" : "2020"}
-                      {(item == 1 || item == 3) && localStorage.getItem("Rol") === "Administrador" && open === item ?
+                      {/* {(item == 1 || item == 3) && localStorage.getItem("Rol") === "Administrador" && open === item ?
                         <IconButton
                           onClick={() => {
                             creaPAE("2023", "6", "PAE_V2.pdf", "/prueba/");
@@ -334,7 +362,7 @@ export const TabPAE = ({
                         >
                           <AddCircleIcon fontSize="small" sx={{ display: "flex", justifyContent: "flex-end" }} />
                         </IconButton>
-                        : ""}
+                        : ""} */}
                     </Typography>
 
 
@@ -367,7 +395,7 @@ export const TabPAE = ({
           componenteSelect==3&&actividadSelect==0?
           "Programa Anual de evaluación 2019, Versión 2":
           "Programa Anual de evaluación 2019, Versión 1"}
-          .Creado el:  
+          .Publicado el:  
           </Typography>
 
           <Typography sx={{ fontFamily: "MontserratMedium", fontSize: "0.7vw", heigh: "1px", marginBottom: "1%", alignContent: "right", textAlign: "right" }}>
@@ -388,7 +416,247 @@ export const TabPAE = ({
                           "16/Abril/2019"}
           </Typography>
           <Grid container item xs={12} sx={{ height: "-webkit-fill-available" }}>
-            <InsertarComponentePDF Nombre={"C" + (componenteSelect + 1) + "A" + (actividadSelect + 1)} />
+            {/* <InsertarComponentePDF Nombre={"C" + (componenteSelect + 1) + "A" + (actividadSelect + 1)} /> */}
+
+
+
+
+
+              {/* TABLA */}
+
+            <Grid
+              container
+              item
+              lg={10}
+              md={9}
+              height="65vh"
+              direction="row"
+              sx={{ backgroundColor: "#FFFF", borderRadius: 5, boxShadow: 5 }}
+            >
+             
+
+              <TableContainer sx={{ borderRadius: 5, height: 450,
+            overflow: "auto",
+            "&::-webkit-scrollbar": {
+              width: ".5vw",
+              mt: 1,
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#edeaea",
+              //outline: "1px solid slategrey",
+              borderRadius: 1,
+            },
+             }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow
+                      sx={{
+                        gridTemplateColumns: "repeat(7,1fr)",
+                      }}
+                    >
+                      {heads.map((head, index) => (
+                        <TableCell
+                          sx={{
+                            backgroundColor: "#edeaea",
+                            fontFamily: "MontserratBold",
+                            borderBottom: 0,
+                            fontSize: "0.8vw",
+                            // fontFamily: "MontserratRegular",
+                            //   fontSize: ".7vw",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                          align="center"
+                          key={index}
+                        >
+                          <TableSortLabel>{head.label}</TableSortLabel>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    {mirsFiltered
+                       .slice(
+                         page * rowsPerPage,
+                         page * rowsPerPage + rowsPerPage
+                       )
+                      .map((row, index) => (
+                        <TableRow>
+                          <TableCell
+                            sx={{
+                              padding: "1px 15px 1px 0",
+                              fontFamily: "MontserratRegular",
+                              fontSize: ".7vw",
+                            }}
+                            align="center"
+                            component="th"
+                            scope="row"
+                          >
+                            {row.AnioFiscal}
+                          </TableCell>
+
+                          <TableCell
+                            sx={{
+                              flexDirection: "row",
+                              display: "grid",
+                              gridTemplateColumns: "repeat(4,1fr)",
+                            }}
+                            align="center"
+                            component="th"
+                            scope="row"
+                          >
+                           
+                              <Tooltip
+                                PopperProps={{
+                                  modifiers: [
+                                    {
+                                      name: "offset",
+                                      options: {
+                                        offset: [0, -13],
+                                      },
+                                    },
+                                  ],
+                                }}
+                                title="DESCARGAR MIR"
+                              >
+                                <span>
+                                  <IconButton
+                                    
+                                    // onClick={() =>
+                                    //   downloadMIR(
+                                    //     row.AnioFiscal,
+                                    //     row.Entidad,
+                                    //     row.Programa,
+                                    //     row.MIR
+                                    //   )
+                                    // }
+                                  >
+                                    <DownloadIcon
+                                      sx={[
+                                        {
+                                          "&:hover": {
+                                            color: "orange",
+                                          },
+                                          width: "1.2vw",
+                                          height: "1.2vw",
+                                        },
+                                      ]}
+                                    />
+                                  </IconButton>
+                                </span>
+                              </Tooltip>
+
+                              {/* <ComentDialogMir
+                                estado={row.Estado}
+                                id={row.Id}
+                                actualizado={actualizaContador}
+                              />
+
+                              <DeleteDialogMIR
+                                disab={
+                                  row.Estado === "En Captura" && validaFecha &&
+                                  localStorage.getItem("Rol") === "Capturador"
+                                    ? false
+                                    : row.Estado === "En Revisión" &&
+                                      localStorage.getItem("Rol") ===
+                                        "Verificador"
+                                    ? false
+                                    : row.Estado === "En Autorización" &&
+                                      localStorage.getItem("Rol") ===
+                                        "Administrador"
+                                    ? false
+                                    : true
+                                }
+                                id={row.Id}
+                                actualizado={actualizaContador}
+                              /> */}
+                              <Tooltip
+                                title="VISUALIZAR"
+                                PopperProps={{
+                                  modifiers: [
+                                    {
+                                      name: "offset",
+                                      options: {
+                                        offset: [0, -13],
+                                      },
+                                    },
+                                  ],
+                                }}
+                              >
+                                <span>
+                                  <IconButton
+                                   
+                                    // onClick={() => {
+                                    //   setMirEdit([
+                                    //     {
+                                    //       Id: row.Id,
+                                    //       AnioFiscal: row.AnioFiscal,
+                                    //       Entidad: row.Entidad,
+                                    //       Programa: row.Programa,
+                                    //       Eje: row.Eje,
+                                    //       Tematica: row.Tematica,
+                                    //       MIR: row.MIR,
+                                    //       Estado: row.Estado,
+                                    //       FechaCreacion: row.FechaCreacion,
+                                    //       CreadoPor: row.CreadoPor,
+                                    //       Conac: row.Conac,
+                                    //       Consecutivo: row.Consecutivo,
+                                    //       Opciones: row.Opciones,
+                                    //     },
+                                    //   ]);
+                                    //   setShowResume(false);
+                                    //   setActionNumber(1);
+                                    // }}
+                                  >
+                                    <LaunchIcon
+                                      sx={[
+                                        {
+                                          "&:hover": {
+                                            color: "blue",
+                                          },
+                                          width: "1.2vw",
+                                          height: "1.2vw",
+                                        },
+                                      ]}
+                                    />
+                                  </IconButton>
+                                </span>
+                              </Tooltip>
+                            
+                          </TableCell>
+                        </TableRow>
+                      ))}
+
+                    {/* ))} */}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+
+              {/* <Box sx={{ width: "100%" }}>
+                 <TablePagination
+                  rowsPerPageOptions={[renglonesPagina]}
+                  component="div"
+                  count={mirs.length}
+                  rowsPerPage={renglonesPagina}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                /> 
+              </Box> */}
+            </Grid>
+
+
+
+
+
+
+
+
+
+
+
+
           </Grid>
         </Grid>
 
@@ -398,3 +666,18 @@ export const TabPAE = ({
     </>
   );
 };
+export interface IIPAE {
+  Id: string;
+  AnioFiscal: string;
+  Entidad: string;
+  Programa: string;
+  Eje: string;
+  Tematica: string;
+  MIR: string;
+  Estado: string;
+  FechaCreacion: string;
+  CreadoPor: string;
+  Conac: string;
+  Consecutivo: String;
+  Opciones: string;
+}
