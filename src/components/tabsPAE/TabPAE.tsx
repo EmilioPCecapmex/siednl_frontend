785f5f64492ad0451badee083892d8929fd8d478
beto081091 <beto_081091@hotmail.com>
2023-10-31 14:37:26 -0600

    PAE

--- src/components/tabsPAE/TabPAE.tsx
+++ src/components/tabsPAE/TabPAE.tsx
@@ -454,9 +454,9 @@ export const TabPAE = ({
       });
   };
 
-  const guardarDoc = (archivo: { archivo: File; nombreArchivo: string }, ruta_inicial: string) => {
+  const guardarDoc = (archivo: { archivo: File; nombreArchivo: string }, perteneceA: string) => {
     const url = new File([archivo.archivo], archivo.nombreArchivo);
-    let ruta = "/SIEDNL_DEV/";
+    let ruta = "/SIEDNL_DEV/"+perteneceA+"/";
     ruta = ((process.env.REACT_APP_DOC_ROUTE || "") + ruta).trim();
     // console.log("ruta:", ruta)
     let dataArray = new FormData();
@@ -509,7 +509,7 @@ export const TabPAE = ({
 
   const handleClickAddPDF = () => {
     if (fileInputRef.current) {
-      guardarDoc({ archivo: (fileInputRef.current.children[0] as HTMLInputElement).files![0], nombreArchivo: (fileInputRef.current.children[0] as HTMLInputElement).files![0].name }, "a");
+      guardarDoc({ archivo: (fileInputRef.current.children[0] as HTMLInputElement).files![0], nombreArchivo: (fileInputRef.current.children[0] as HTMLInputElement).files![0].name }, PerteneceAValue.replace(" ","_"));
       fileInputRef.current.click();
       creaPAE((fileInputRef.current.children[0] as HTMLInputElement).files![0].name, (process.env.REACT_APP_DOC_ROUTE || "") + "/SIEDNL_DEV/", componenteSelect === 0 ? "2022" : componenteSelect === 1 ? "2021" : "2020", PerteneceAValue)
     }
@@ -800,7 +800,7 @@ export const TabPAE = ({
                             scope="row"
                           >
                             <VisualizarPAE
-                              ruta={(process.env.REACT_APP_DOC_ROUTE || "") + "/SIEDNL_DEV/"}
+                              ruta={(process.env.REACT_APP_DOC_ROUTE || "") + "/SIEDNL_DEV/" + PerteneceAValue.replace(" ","_") + "/"}
                               nombre={row.Nombre}
                               tipo={"pdf"}
                               anio={componenteSelect === 0 ? "2022" : componenteSelect === 1 ? "2021" : "2020"}
