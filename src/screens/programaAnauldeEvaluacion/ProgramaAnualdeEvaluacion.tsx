722df5c0004a6bce6b9d15a74ca9c2c003b9e2d8
Israelcacapmex <124214078+Israelcacapmex@users.noreply.github.com>
2024-02-19 11:06:21 -0600

    cambios beneficiario, notificaciones 50%

--- src/screens/programaAnauldeEvaluacion/ProgramaAnualdeEvaluacion.tsx
+++ src/screens/programaAnauldeEvaluacion/ProgramaAnualdeEvaluacion.tsx
@@ -1,38 +1,33 @@
-import {
-  
-  Grid,
-  
-} from "@mui/material";
-import {
-  LateralMenu,
-
-} from "../../components/lateralMenu/LateralMenu";
+import { Grid } from "@mui/material";
+import { LateralMenu } from "../../components/lateralMenu/LateralMenu";
 //import { useNavigate } from "react-router-dom";
 //import { Header } from "../../components/header/Header";
 import React, { useState } from "react";
 import CapturaPAE from "../../components/tabsPAE/CapturaPAE";
-            
+
 export const ProgramaAnualEvaluacion = () => {
   //const navigate = useNavigate();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [actionNumber, setActionNumber] = useState(0);
-
+  
 
   return (
     <Grid container justifyContent={"space-between"}>
       <Grid
-       item
-       xl={12}
-       height={"7vh"}
+        item
+        xl={12}
+        lg={12}
+        md={12}
+        sm={12}
+        xs={12}
+        sx={{ height: "7vh", whitespace: "nowrap" }}
         //</Grid>sx={{ mr: showResume ? 5 : 0 }}
       >
-        <LateralMenu
-          selection={"PAE"}
-          actionNumber={actionNumber}
-        />
+        <LateralMenu selection={"PAE"} actionNumber={actionNumber} />
       </Grid>
 
-      <Grid justifyContent={"center"}
+      <Grid
+        justifyContent={"center"}
         display={"flex"}
         container
         height={"93vh"}
@@ -41,37 +36,30 @@ export const ProgramaAnualEvaluacion = () => {
         xl={12}
         lg={12}
         md={12}
-        sm={7.5}
-        xs={6}
-        sx={{ backgroundColor: "white", }}
+        sm={12}
+        xs={12}
+       // sx={{ backgroundColor: "white" }}
       >
-        {/* <Grid sx={{ height: "8vh", marginLeft: "4vw" }}>
-          <Header
-            details={{
-              name1: "Inicio",
-              path1: "../home",
-              name2: "Programa Anual de Evaluación",
-              path2: "../ProgramaAnualEvaluacion",
-              name3: "",
-            }}
-          />
-
-        
-
-      </Grid> */}
-      <Grid
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-              height: "92vh",
-            }}
-            gridArea={"main"}
-          >
-            <CapturaPAE />
-          </Grid>
-    </Grid>
+       
+        <Grid
+          item
+          xl={10}
+          lg={10}
+          md={10}
+          sm={10}
+          xs={10}
+          sx={{
+            backgroundColor: "#FFFF",
+            borderRadius: 5,
+            boxShadow: 5,
+            height: "92vh",
+            direction: "row",
+          }}
+         // gridArea={"main"}
+        >
+          <CapturaPAE />
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
