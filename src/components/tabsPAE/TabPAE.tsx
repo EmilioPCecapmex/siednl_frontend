2b993fa36252b73c9df67672c49dacc9fca86540
Israelcacapmex <124214078+Israelcacapmex@users.noreply.github.com>
2024-02-27 11:59:21 -0600

    botones de eliminar

--- src/components/tabsPAE/TabPAE.tsx
+++ src/components/tabsPAE/TabPAE.tsx
@@ -60,12 +60,10 @@ export const TabPAE = ({
     setRegistrosFiltrados(registros);
 
     if (TabSelect !== "Todos los Documentos") {
-      
-      
       let aux = registros.filter((registro) => {
         return registro.PerteneceA === tabsShow;
       });
-     
+
       setRegistrosFiltrados(aux);
     }
     setProgressBar(false);
@@ -272,26 +270,27 @@ export const TabPAE = ({
                 justifyContent: "flex-end",
               }}
             >
-              <Button variant="outlined" onClick={()=>{handleClickOpen()}}
-                >
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  handleClickOpen();
+                }}
+              >
                 Cargar Archivo
               </Button>
-              {open ?
+              {open ? (
                 <DialogCargaArchivo
                   Tabs={Tabs.filter((tab) => tab !== "Todos los Documentos")}
                   Tab={
                     TabSelect === "Todos los Documentos" ? Tabs[1] : tabsShow
                   }
-                  updateData={() => {actuaizarDatos()
-                  
-                  }
-                    
-                  }
+                  updateData={() => {
+                    actuaizarDatos();
+                  }}
                   open={open}
                   setOpen={setOpen}
-                />: null
-              } 
-
+                />
+              ) : null}
             </Grid>
           ) : (
             ""
@@ -509,12 +508,15 @@ export const TabPAE = ({
                             </IconButton>
                           </span>
                         </Tooltip>
-                        <DeleteDialogPAE
-                          id={row.Id}
-                          updateData={() => {
-                            actuaizarDatos();
-                          }}
-                        />
+
+                        {localStorage.getItem("Rol") === "Administrador" ? (
+                          <DeleteDialogPAE
+                            id={row.Id}
+                            updateData={() => {
+                              actuaizarDatos();
+                            }}
+                          />
+                        ) : null}
                       </TableCell>
                     </TableRow>
                   ))}
